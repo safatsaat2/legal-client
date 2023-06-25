3c867cfaff2eec76c0f959b57e5de50ebbc5f097
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-06-25 02:26:08 -0700

    features added

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,7 +1,7 @@
 
 const Header = ({heading, subHeading}) => {
     return (
-        <div className="text-center mt-24">
+        <div className="text-center mt-24 mb-11">
             <h1 className="font-header font-bold text-color text-5xl mb-3">{heading}</h1>
             <p className="w-[431px] mx-auto font-para text-sm text-sec-col">{subHeading}</p>
         </div>
--- src/Pages/Home/Features/Features.jsx
+++ src/Pages/Home/Features/Features.jsx
@@ -1,16 +1,98 @@
+import { Icon } from "@iconify/react";
 import Header from "../../../Component/Header";
 
 const Features = () => {
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center w-full mx-7">
       <Header
         heading="Every Client Matters"
         subHeading="Problems trying to resolve the conflict between 
         the two major realms of Classical physics: Newtonian mechanics "
       />
-      <div className='grid grid-cols-3'>
-        <div>
-
+      <div className="grid grid-cols-3 gap-8 w-full mx-auto">
+        <div className="border-2 border-[#E8E8E8] rounded-md  w-80">
+          <div className="mx-10 my-9">
+            <div className="flex items-center gap-2">
+              <Icon className="text-5xl" icon="pepicons-pencil:hammer-claw" />
+              <h4 className=" text-lg font-header font-bold">Family Law</h4>
+            </div>
+            <p className="text-sec-col mt-4">
+              the quick fox jumps over the lazy dog the quick fox jumps over the
+              lazy dog
+            </p>
+          </div>
+        </div>
+        <div className="border-2 border-[#E8E8E8] rounded-md  w-80">
+          <div className="mx-10 my-9">
+            <div className="flex items-center gap-2">
+              <Icon
+                className="text-5xl"
+                icon="streamline:legal-justice-hammer-hammer-work-legal-mallet-office-company-gavel-justice-judge-arbitration-court"
+              />
+              <h4 className=" text-lg font-header font-bold">Business Law</h4>
+            </div>
+            <p className="text-sec-col mt-4">
+              the quick fox jumps over the lazy dog the quick fox jumps over the
+              lazy dog
+            </p>
+          </div>
+        </div>
+        <div className="border-2 border-[#E8E8E8] rounded-md  w-80">
+          <div className="mx-10 my-9">
+            <div className="flex items-center gap-2">
+              <Icon
+                className="text-5xl"
+                icon="streamline:legal-justice-scale-1-office-work-legal-scale-justice-company-arbitration-balance-court"
+              />
+              <h4 className=" text-lg font-header font-bold">
+                Trust & Estates
+              </h4>
+            </div>
+            <p className="text-sec-col mt-4">
+              the quick fox jumps over the lazy dog the quick fox jumps over the
+              lazy dog
+            </p>
+          </div>
+        </div>
+        <div className="border-2 border-[#E8E8E8] rounded-md  w-80">
+          <div className="mx-10 my-9">
+            <div className="flex items-center gap-2">
+              <Icon className="text-5xl" icon="fluent:textbox-16-regular" />
+              <h4 className=" text-lg font-header font-bold">
+                Civil Litigation
+              </h4>
+            </div>
+            <p className="text-sec-col mt-4">
+              the quick fox jumps over the lazy dog the quick fox jumps over the
+              lazy dog
+            </p>
+          </div>
+        </div>
+        <div className="border-2 border-[#E8E8E8] rounded-md  w-80">
+          <div className="mx-10 my-9">
+            <div className="flex items-center gap-2">
+              <Icon className="text-5xl" icon="mdi:legal" />
+              <h4 className=" text-lg font-header font-bold">
+                Trust & Estates
+              </h4>
+            </div>
+            <p className="text-sec-col mt-4">
+              the quick fox jumps over the lazy dog the quick fox jumps over the
+              lazy dog
+            </p>
+          </div>
+        </div>
+        <div className="border-2 border-[#E8E8E8] rounded-md  w-80">
+          <div className="mx-10 my-9">
+            <div className="flex items-center gap-2">
+              <Icon className="text-5xl" icon="pepicons-pencil:hammer-claw" />
+              <h4 className=" text-lg font-header font-bold">Family Law</h4>
+            </div>
+            <p className="text-sec-col mt-4">
+              the quick fox jumps over the lazy dog the quick fox jumps over the
+              lazy dog
+            </p>
+          </div>
         </div>
       </div>
     </div>
